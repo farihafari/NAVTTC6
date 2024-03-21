9f0f5d41a2a1e2cfc113ff771160811ead48d602
farihafari <farihafari594@gmail.com>
2024-03-21 14:27:43 +0500

    imgpreview

---
+++ javascript/assets/js/function.js
@@ -0,0 +1,25 @@
+function userDetails(){
+    let inputData = document.querySelectorAll("input");
+    // console.log(inputData);
+    let x ="";
+// let conditionValue = inputData[]
+    let list = document.querySelector("#list");
+    // console.log(inputData.value)
+    i.forEach(function(index,val){
+        console.log(index)
+        // if(index.value == ""){
+        // }else{
+        //     +`</li>`
+       
+        // }
+        // console.log(index.value);
+    })
+for(let i=0;i<inputData.length;i++){
+    x+=`<li>`+inputData[val].value
+  
+}
+
+list.innerHTML=x;
+    // alert("function without parameter")
+}
+// userDetails();
---
+++ javascript/assets/js/functionpm.js
@@ -0,0 +1,10 @@
+function sum(a,b){
+    return a+b;
+}
+let x =sum("2","8");
+console.log(x);
+function imgPreview(Simg){
+    let Bimg = document.querySelector("#bImg");
+    // let Simg = document.querySelector('#sImg');
+    BimWg.src 
+}
